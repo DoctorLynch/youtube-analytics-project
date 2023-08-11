c32bc95c3070d7c4835c1e3e680a7b0be330ba6a
DoctorLynch <doctor_linchenko@mail.ru>
2023-08-11 23:52:03 +0300

    HW6

--- src/video.py
+++ src/video.py
@@ -8,22 +8,33 @@ class Video(APIMixin):
         self._init_from_api()
 
     def _init_from_api(self):
-        video_response = self.get_service().videos().list(
-            part='snippet,statistics', id=self.__id_video).execute()
+        try:
+            video_response = self.get_service().videos().list(
+                part='snippet,statistics', id=self.__id_video).execute()
 
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/channel/{self.__id_video}'
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/channel/{self.__id_video}'
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+
+
+
+
 
     def __str__(self):
-        return self.title
+        return self.__id_video
 
 
 class PLVideo(Video):
     def __init__(self, id_video, id_playlist):
         super().__init__(id_video)
         self.id_playlist = id_playlist
+
 
 
 
